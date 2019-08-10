bf18a945c3073326915fee193fb10cb942188373
Carlos Catrian <carlos.catrian@ensys.cl>
2019-08-10 19:57:23 -0400

    se modifica db

--- MyLeasing.Web/Controllers/OwnersController.cs
+++ MyLeasing.Web/Controllers/OwnersController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyLeasing.Web.Data;
 using MyLeasing.Web.Data.Entities;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyLeasing.Web.Controllers
 {
@@ -51,7 +54,7 @@ namespace MyLeasing.Web.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Document,FirstName,LastName,FixedPhone,CellPhone,Address")] Owner owner)
+        public async Task<IActionResult> Create([Bind("Id")] Owner owner)
         {
             if (ModelState.IsValid)
             {
@@ -83,7 +86,7 @@ namespace MyLeasing.Web.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Document,FirstName,LastName,FixedPhone,CellPhone,Address")] Owner owner)
+        public async Task<IActionResult> Edit(int id, [Bind("Id")] Owner owner)
         {
             if (id != owner.Id)
             {
